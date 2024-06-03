5294f5fcef6799ec0df28c1a6f1ec8462087a1fa
luciarol <luciarol87@gmail.com>
2024-06-03 17:31:00 +0200

    rutas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ClienteController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MineralController;
@@ -47,20 +46,6 @@ Route::post('/crear-mineral', [MineralController::class, 'crearmineral'])->name(
 Route::post('/guardar-mineral', [MineralController::class, 'guardarmineral'])->name('guardar-mineral');
 
 
-
-
-Route::get('/crear-cliente', [ClienteController::class, 'create'])->name('crear-cliente');
-Route::get('/clientes/create', [ClienteController::class, 'create'])->name('clientes.create');
-
-Route::post('/clientes', [ClienteController::class, 'store'])->name('clientes.store');
-
-
-Route::get('/ver-clientes', [ClienteController::class, 'index'])->name('clientes.index');
-
-
-Route::delete('/clientes/{cliente}', [ClienteController::class, 'destroy'])->name('clientes.destroy');
-
-Route::put('/clientes/{cliente}', [ClienteController::class, 'update'])->name('clientes.update');
 
 
 
